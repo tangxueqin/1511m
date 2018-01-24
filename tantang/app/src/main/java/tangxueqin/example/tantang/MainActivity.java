956f646acb10198543ecbd8c8851f0090bb91be0
unknown <@DESKTOP-J1SLP4B.(none)>
2018-01-24 19:27:09 +0800

    cjscjkcn

---
+++ tantang/app/src/main/java/tangxueqin/example/tantang/MainActivity.java
@@ -0,0 +1,14 @@
+package tangxueqin.example.tantang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次提交代码
+    }
+}
